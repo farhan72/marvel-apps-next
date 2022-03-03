f8fe9ccbab443801c7b9743151f77843f64d7d89
alfiy <alfiibnu84@gmail.com>
2022-03-03 23:06:16 +0700

    init_directories_pattern

---
+++ pages/login/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next';
+import React from 'react';
+
+const Index: NextPage = () => {
+    return (
+        <div>
+            Login Page
+        </div>
+    );
+}
+
+export default Index;
